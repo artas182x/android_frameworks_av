9a0c5ea17f56b6da5056c00d3db90bcf00836916
Hashcode <hashcode0f@gmail.com>
2013-11-12 14:31:24 -0800

    [3/3] av: Allow custom DOMX source instead of Google DOMX default
    Part 3 of 3 patches: To allow omap4 devices to use custom "domx" source
    via a new BoardConfig.mk item:
    TI_CUSTOM_DOMX_PATH := device/<manufacturer>/<device-name>/domx
    
    This setting provides for 3 changes during the build:
    1. In hardware/ti/omap4xxx this settings stops standard Google domx
    source from being built and changes the domx reference for tiutils.
    2. In frameworks/base it changes the default openmax references for
    frameworks/base/media/jni/mediaeditor/Android.mk to the new location
    3. In frameworks/av changes the openmax references in 5 places, and adds
    new includes in ACodec.cpp, CameraSource.cpp and OMXCodec.cpp
    
    This commit is based on cherry-pick from omapzoom (with a more descriptive
    BoardConfig setting name):
    http://www.omapzoom.org/?p=platform/frameworks/base.git;a=commit;h=f7f929ff477955333bf7e7413a81a66d68
    
    Change-Id: I67dee7e077cd850acad6a0d4afb70218a8b9f2cd
    
    Conflicts:
    	media/libstagefright/Android.mk

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -70,7 +70,6 @@ LOCAL_SRC_FILES:=                         \
 LOCAL_C_INCLUDES:= \
         $(TOP)/frameworks/av/include/media/stagefright/timedtext \
         $(TOP)/frameworks/native/include/media/hardware \
-        $(TOP)/frameworks/native/include/media/openmax \
         $(TOP)/frameworks/native/services/connectivitymanager \
         $(TOP)/external/flac/include \
         $(TOP)/external/tremolo \
@@ -79,6 +78,13 @@ LOCAL_C_INCLUDES:= \
 ifneq ($(TI_CUSTOM_DOMX_PATH),)
 LOCAL_C_INCLUDES += $(TI_CUSTOM_DOMX_PATH)/omx_core/inc
 LOCAL_CPPFLAGS += -DUSE_TI_CUSTOM_DOMX
+else
+LOCAL_C_INCLUDES += $(TOP)/frameworks/native/include/media/openmax
+endif
+
+ifneq ($(TARGET_QCOM_MEDIA_VARIANT),)
+LOCAL_C_INCLUDES += \
+        $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
 else
 LOCAL_C_INCLUDES += $(TOP)/frameworks/native/include/media/openmax
 endif
